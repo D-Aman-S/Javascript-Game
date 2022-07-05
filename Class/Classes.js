61aa30a1c60176df1177975e110faa7bb314d9fd
daring-jackson <amans6963@gmail.com>
2022-07-05 08:39:33 +0530

    maybe final commit

--- Class/Classes.js
+++ Class/Classes.js
@@ -1,4 +1,58 @@
-class Sprite{
+//original sprite
+class Sprite {
+    constructor({
+      position,
+      imageSrc,
+      scale = 1,
+      framesMax = 1,
+      offset = { x: 0, y: 0 }
+    }) {
+      this.position = position
+      this.width = 50
+      this.height = 150
+      this.image = new Image()
+      this.image.src = imageSrc
+      this.scale = scale
+      this.framesMax = framesMax
+      this.framesCurrent = 0
+      this.framesElapsed = 0
+      this.framesHold = 5
+      this.offset = offset
+    }
+  
+    draw() {
+      context.drawImage(
+        this.image,
+        this.framesCurrent * (this.image.width / this.framesMax),
+        0,
+        this.image.width / this.framesMax,
+        this.image.height,
+        this.position.x - this.offset.x,
+        this.position.y - this.offset.y,
+        (this.image.width / this.framesMax) * this.scale,
+        this.image.height * this.scale
+      )
+    }
+  
+    animateFrames() {
+      this.framesElapsed++
+  
+      if (this.framesElapsed % this.framesHold === 0) {
+        if (this.framesCurrent < this.framesMax - 1) {
+          this.framesCurrent++
+        } else {
+          this.framesCurrent = 0
+        }
+      }
+    }
+  
+    update() {
+      this.draw()
+      this.animateFrames()
+    }
+  }
+//my change for animation
+class AnimationObjects{
     constructor({position, imageSrc, scale = 1, numofimg=1}){
         this.position = position;
         this.height = 150;
@@ -9,7 +63,7 @@ class Sprite{
         this.scale=scale
         this.numofimg=numofimg
         this.imageCounter=0;
-        this.holdFrame=15
+        this.holdFrame=50
         this.framerCounter=0
         console.log(this.image.src);
 
--- Class/Objects.js
+++ Class/Objects.js
@@ -1,6 +1,17 @@
-//creating a background object
+//sprite shop
+const shop = new Sprite({
+    position: {
+      x: 620,
+      y: 310
+    },
+    imageSrc: 'Props/shop.png',
+    scale: 2.10,
+    framesMax: 6
+  })
 
-const background = new Sprite({
+
+//creating a background object-not in video
+const background = new AnimationObjects({
     position:{
       x:0,
       y:0
@@ -8,43 +19,43 @@ const background = new Sprite({
     imageSrc:'Props/bfight.png' 
   })
   //plant animation
-  const plant = new Sprite({
+  const plant = new AnimationObjects({
       position:{
         x:900,
         y:480
       } ,
       imageSrc:'Plant/?.png' ,
       scale:0.08,
-      numofimg:9
+      numofimg:3
     })
-    const plant2 = new Sprite({
+    const plant2 = new AnimationObjects({
       position:{
         x:950,
         y:480
       } ,
       imageSrc:'Plant/?.png' ,
       scale:0.08,
-      numofimg:9
+      numofimg:3
     })
-    const plant3 = new Sprite({
+    const plant3 = new AnimationObjects({
       position:{
         x:10,
         y:480
       } ,
       imageSrc:'Plant2/?.png' ,
       scale:0.36,
-      numofimg:10
+      numofimg:4
     })
-    const plant4 = new Sprite({
+    const plant4 = new AnimationObjects({
       position:{
         x:-40,
         y:480
       } ,
       imageSrc:'Plant2/?.png' ,
       scale:0.36,
-      numofimg:10
+      numofimg:4
     })
-    const blood = new Sprite({
+    const blood = new AnimationObjects({
       position:{
         x:-50,
         y:-50
@@ -53,24 +64,24 @@ const background = new Sprite({
       scale:2,
       numofimg:16
     })
-    const blood1 = new Sprite({
+    const blood1 = new AnimationObjects({
         position:{
           x:880,
           y:-50
         } ,
         imageSrc:'Blood/5/1_?.png' ,
         scale:2,
-        numofimg:10
+        numofimg:16
       })
-      const tree = new Sprite({
+      const tree = new AnimationObjects({
         position:{
-          x:715,
+          x:760,
           y:198
         } ,
         imageSrc:'Props/tree.png' ,
         scale:1.5
       })
-      const house = new Sprite({
+      const house = new AnimationObjects({
         position:{
           x:-30,
           y:250
@@ -78,7 +89,7 @@ const background = new Sprite({
         imageSrc:'Props/house.png' ,
         scale:1.5
       })
-      const floor = new Sprite({
+      const floor = new AnimationObjects({
         position:{
           x:350,
           y:552
